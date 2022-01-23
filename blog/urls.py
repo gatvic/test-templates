3c00746e4152b0393b888319c4732595a1c223c1
gatvic <gatvic@yandex.ru>
2022-01-23 23:11:46 +0300

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index, choice
+from . import views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('choice_plant', choice, name='choice'),
+    path('', views.index, name='index'),
+    path('blog/', views.choice, name='choice'),
 ]
