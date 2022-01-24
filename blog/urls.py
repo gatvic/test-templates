25f9091fd8cdd1d60818a971f52d1f15494d3a91
gatvic <gatvic@yandex.ru>
2022-01-24 22:28:39 +0300

    fix title

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('blog/', views.choice, name='choice'),
+    path('', views.choice, name='choice'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,11 @@ def index(request):
 
 def choice(request):
     return render(request, 'blog/choice_plant.html', {})
+
+
+def ad_plant(request):
+    return render(request, 'blog/ad_plant.html', {})
+
+
+def ad_sub(request):
+    return render(request, 'blog/ad_sub.html', {})
