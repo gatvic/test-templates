82edab35dc8b4574c03cd4cbd29091caa413682a
gatvic <gatvic@yandex.ru>
2022-01-24 22:32:55 +0300

    add urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.choice, name='choice'),
+    path('blog/', views.choice, name='choice'),
+    path('blog/', views.ad_plant, name='ad_plant'),
+    path('blog/', views.ad_sub, name='ad_sub'),
 ]
